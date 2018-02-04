d10d876c3f62c2bb9da732fa5fc20f60a6d2b53a
paszin <pascalslogin@gmail.com>
2018-02-04 03:41:14 +0100

    added constraints

--- src/js/tools/texture_canvasdrawer_canvasstyle.js
+++ src/js/tools/texture_canvasdrawer_canvasstyle.js
@@ -13,7 +13,7 @@ class TextureCanvasDrawerC {
   constructor(canvas) {
 
     this.canvas = canvas;
-    this.cellCount = 0;
+    this.cellCount = 1;
     this.stage = new createjs.Stage(this.canvas[0]);
 
     this.lines = [];
@@ -60,6 +60,44 @@ class TextureCanvasDrawerC {
     this.pathCommands = [];
   }
 
+  addCell() {
+
+			this.dimensions.y += this.cellHeight;
+      this.cellCount++;
+
+			this.canvas.attr({height:this.dimensions.y});
+			this.middleLine.scaleY = this.dimensions.y/this.cellHeight;
+
+			//add a vertical line
+			var verticalLine = new createjs.Shape();
+			this.stage.addChild(verticalLine);
+			//middleLine.graphics.setStrokeDash([2,2]);
+			verticalLine.graphics.setStrokeStyle([2, 2]).beginStroke("rgba(0.5,0.5,0.5,0.1)");
+			verticalLine.graphics.moveTo(0, this.dimensions.y-this.cellHeight);
+			verticalLine.graphics.lineTo(this.dimensions.x, this.dimensions.y-this.cellHeight);
+			verticalLine.graphics.endStroke();
+
+			this.stage.update();
+		}
+
+    removeCell() {
+      this.dimensions.y -= this.cellHeight;
+      this.cellCount--;
+
+      this.canvas.attr({height: this.dimensions.y});
+      this.middleLine.scaleY = this.dimensions.y/this.cellHeight;
+
+      for (var i = 0; i < this.pathCommands.length; i++) {
+        this.pathCommands[i].y = this.pathCommands[i].y/(this.dimensions.y + this.cellHeight) * this.dimensions.y;
+      }
+      for (var i = 0; i < this.points.length; i++) {
+        this.points[i].y = this.points[i].y/(this.dimensions.y + this.cellHeight) * this.dimensions.y;
+      }
+
+      this.drawPath();
+
+    }
+
   load(coordArray) {
 
     this.initCanvas();
@@ -67,7 +105,6 @@ class TextureCanvasDrawerC {
     if (!coordArray || coordArray.length == 0) {
       return
     }
-    var self = this;
     var width = this.dimensions.x;
     var height = this.dimensions.y;
     var line = new createjs.Shape();
@@ -76,8 +113,12 @@ class TextureCanvasDrawerC {
       this.pathCommands.push(line.graphics.lineTo(width * coordArray[i][0], height * coordArray[i][1]).command);
     }
     this.drawPath();
-    this.pathCommands.forEach(function(c) {
-      self.addPoint(c.x, c.y);
+    this.pathCommands.forEach((c, i) => {
+      var move = (x, y) => [x, y];
+      if (i == 0 || i == this.pathCommands.length-1) {
+        move = (x, y) => [x, Math.min(i, 1)*this.cellCount*this.dimensions.y];
+      }
+      this.addPoint(c.x, c.y, move);
       //.cpx, .cpy for quadratic curve
     });
     this.stage.update();
@@ -116,7 +157,7 @@ class TextureCanvasDrawerC {
 
   }
 
-  addPoint(x, y) {
+  addPoint(x, y, move = (x, y) => [x, y]) {
     var circle = new createjs.Shape();
     circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 8);
     circle.x = x;
@@ -125,11 +166,12 @@ class TextureCanvasDrawerC {
     circle.on("pressmove", function(evt) {
       self.movingDot = true;
       var command = self.getCommand(evt.currentTarget.x, evt.currentTarget.y);
-      command.x = evt.stageX;
-      command.y = evt.stageY;
+      var xy = move(evt.stageX, evt.stageY);
+      command.x = xy[0];
+      command.y = xy[1];
       self.drawPath(this.pathCommands);
-      evt.currentTarget.x = evt.stageX;
-      evt.currentTarget.y = evt.stageY;
+      evt.currentTarget.x = xy[0];
+      evt.currentTarget.y = xy[1];
       self.stage.update();
     });
     circle.on("pressup", function(evt) {
@@ -209,128 +251,7 @@ class TextureCanvasDrawerC {
     this.middleLine.scaleY = this.dimensions.y/this.cellHeight;
   }
 
-  TextureCanvasDrawer.prototype.addCell = function() {
 
-			this.dimensions.y += this.cellHeight;
-      this.cellCount++;
-
-			this.canvas.attr({height:this.dimensions.y});
-			this.middleLine.scaleY = this.dimensions.y/this.cellHeight;
-
-			//add a vertical line
-			var verticalLine = new createjs.Shape();
-			this.stage.addChild(verticalLine);
-			//middleLine.graphics.setStrokeDash([2,2]);
-			verticalLine.graphics.setStrokeStyle([2, 2]).beginStroke("rgba(0.5,0.5,0.5,0.1)");
-			verticalLine.graphics.moveTo(0, this.dimensions.y-this.cellHeight);
-			verticalLine.graphics.lineTo(this.dimensions.x, this.dimensions.y-this.cellHeight);
-			verticalLine.graphics.endStroke();
-
-			this.stage.update();
-		}
-
-  TextureCanvasDrawer.prototype.removeCell = function() {
-    this.dimensions.y -= this.cellHeight;
-    this.cellCount--;
-
-    this.canvas.attr({height: this.dimensions.y});
-    this.middleLine.scaleY = this.dimensions.y/this.cellHeight;
-
-    for (var i = 0; i < this.pathCommands.length; i++) {
-      this.pathCommands[i].y = this.pathCommands[i].y/(this.dimensions.y + this.cellHeight) * this.dimensions.y;
-    }
-    for (var i = 0; i < this.points.length; i++) {
-      this.points[i].y = this.points[i].y/(this.dimensions.y + this.cellHeight) * this.dimensions.y;
-    }
-
-    this.drawPath();
-
-  }
-
-  }
-
-
-  TextureCanvasDrawer.prototype.getCommand = function(x, y) {
-    for (var i = 0; i < this.pathCommands.length; i++) {
-      if (this.pathCommands[i].x == x && this.pathCommands[i].y == y) {
-        return this.pathCommands[i];
-      }
-    }
-  }
-
-  TextureCanvasDrawer.prototype.addPoint = function(x, y) {
-    var circle = new createjs.Shape();
-    circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 8);
-    circle.x = x;
-    circle.y = y;
-    var self = this;
-    circle.on("pressmove", function(evt) {
-      self.movingDot = true;
-      var command = self.getCommand(evt.currentTarget.x, evt.currentTarget.y);
-      command.x = evt.stageX;
-      command.y = evt.stageY;
-      self.drawPath(this.pathCommands);
-      evt.currentTarget.x = evt.stageX;
-      evt.currentTarget.y = evt.stageY;
-      self.stage.update();
-    });
-    circle.on("pressup", function(evt) {
-      self.movingDot = false;
-    });
-    this.stage.addChild(circle);
-    this.points.push(circle);
-  }
-
-  TextureCanvasDrawer.prototype.getPoint = function(t) {
-    //t is in [0;1]
-    //1. calculate length of whole path
-    var length = 0;
-    var parts = [0]; //array with total length till point i
-    for (var i = 1; i < this.pathCommands.length; i++) {
-      var v1 = new THREE.Vector2(this.pathCommands[i - 1].x, this.pathCommands[i - 1].y);
-      var v2 = new THREE.Vector2(this.pathCommands[i].x, this.pathCommands[i].y);
-
-      var distance = v2.distanceTo(v1);
-      length += distance;
-      parts.push(length);
-    }
-
-
-    //2. find the right segment
-    //normalisieren with dimension.y (100)
-    var i = 0;
-    for (i; i < parts.length; i++) {
-      if (parts[i] == t * length) {
-        return new THREE.Vector3(this.pathCommands[i].x / this.dimensions.y, 0, this.pathCommands[i].y / this.dimensions.y);
-      }
-      if (parts[i] > t * length) {
-        //interpolate
-        var start = [this.pathCommands[i - 1].x / this.dimensions.y, this.pathCommands[i - 1].y / this.dimensions.y];
-        var end = [this.pathCommands[i].x / this.dimensions.y, this.pathCommands[i].y / this.dimensions.y];
-        var point = [t * (end[0] - start[0]) + start[0], t * (end[1] - start[1]) + start[1]];
-        return new THREE.Vector3(point[0], 0, point[1]);
-      }
-    }
-
-  }
-
-  TextureCanvasDrawer.prototype.getDrawing = function() {
-    var coordArray = [];
-    for (var i = 0; i < this.pathCommands.length; i++) {
-      coordArray.push([this.pathCommands[i].x/this.dimensions.x, this.pathCommands[i].y/this.dimensions.y]);
-    }
-    return coordArray;
-  }
-
-  TextureCanvasDrawer.prototype.getImage = function() {
-    var image = new Image();
-    image.src = this.canvas[0].toDataURL("image/png");
-    return image;
-  }
-
-  return TextureCanvasDrawer;
-
-})();
 */
 
 module.exports = TextureCanvasDrawerC;
